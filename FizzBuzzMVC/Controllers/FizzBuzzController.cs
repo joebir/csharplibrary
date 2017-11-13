29eba0684f50d5fed730e8f671e381672f1abd11
Joe Bir <joe.bir.code@gmail.com>
2017-11-13 13:19:14 -0500

    fizzbuzz mvc

---
+++ FizzBuzzMVC/Controllers/FizzBuzzController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FizzBuzzMVC.Controllers
+{
+    public class FizzBuzzController : Controller
+    {
+        private string ToFizzBuzz(int num)
+        {
+            var isDivisibleByFive = num % 5 == 0;
+            var isDivisibleByThree = num % 3 == 0;
+
+            if (isDivisibleByFive && isDivisibleByThree) return "FizzBuzz";
+            if (isDivisibleByFive) return "Buzz";
+            if (isDivisibleByThree) return "Fizz";
+
+            return $"{num}";
+        }
+
+        // GET: FizzBuzz
+        public ActionResult Index()
+        {
+            var model = Enumerable.Range(1, 100).Select(ToFizzBuzz);
+            return View(model);
+        }
+    }
+}
